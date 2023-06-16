1c4ffc86de986aedc1cc6c4f90e8f68ed015c710
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-06-16 10:58:15 -0300

    Create 1

---
+++ java/Javafunctions.js
@@ -0,0 +1,63 @@
+//numero random
+function numberRadom(max, min, qt){
+    let nuns = [];
+    for(var i=0; i<qt; i++){
+        nuns[i] = Math.round((Math.random() * max ) + min);
+    }
+    return nuns;
+};
+
+// codigo com numeros e posição aleatória
+function Codigo(digito, val){
+    let poss = [];
+    for(var i=0; i<digito; i++){
+        poss[i] = Math.round((Math.random() *  digito) + 0);
+    }
+    let res = '';
+    for(var i=0; i<poss.length; i++){
+        res += val[poss[i]];
+    }
+    return res;
+}
+
+//img preview
+function PreViewImg(input, imgPreview){
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(imgPreview).attr('style', "background-image: url('"+e.target.result+"');");
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//scroll
+function scroll(x, y){
+	window.scrollTo(x, y);
+}
+
+//mostrar e ocutar divs, em array
+function mostrar(ocu, chave){
+    for (var i=0; i<ocu.length; i++){	
+		if (i<chave){
+			ocultar(ocu[i], 0);	
+		}else{
+			ocultar(ocu[i], 1);
+		};
+	};
+};
+
+//ocultar e mostra div
+function ocultar(obj, es){
+	let div = document.querySelector(obj);
+	if(es==1){
+		div.style.display = 'flex';
+	}else{
+		div.style.display = 'none';
+	};
+};
+
+//redirecionar pagina
+function redirect(page) {
+    window.location.href = page;
+}
