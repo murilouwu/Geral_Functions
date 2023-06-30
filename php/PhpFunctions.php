9b8a6bc7fc1133fb8f7f5d3a0b7e4042fbb02c42
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-06-30 12:25:47 -0300

    Update PhpFunctions.php

--- php/PhpFunctions.php
+++ php/PhpFunctions.php
@@ -11,58 +11,83 @@ $conn = mysqli_connect($db['host'], $db['user'], $db['pass'], $db['nm']) or die 
 function InsertBd($Infs, $Dts, $Vls, $Vers, $Configs, $Up){
     /*
         $Infs = array('nm_table', );
-        $Dts = array('date1', 'date2', ...);
-        $Vls = array('insert_value1','insert_value2'...);
+        $Dts = array('date1', 'date2', ...); 
+        $Vls = array('insert_value1','insert_value2'...); if isset(file){ $Vls = array('insert_value1', 'insert_value2', ...['fileName0','fileName1','fileName2',...]); }
         $Vers = array(0, 1, 2..); key from var Dts
-        $Configs = array(array('OR ou AND 0', 'OR ou AND 1'...), );
+        $Configs = array(array('OR ou AND 0', 'OR ou AND 1'...), 'ERROR'=>array('erro1', 'erro2', ..), 'FilesPast'=>array('FilePastSave0','FilePastSave1', ...));
         $Up = array($file0, $file1...);
     */
     $VersToText = '';
-    for ($i=0; $i<count($Vers); $i++){
-        if($i==0){
-            $VersToText = $VersToText.$Dts[$Vers[$i]].'= '.$Vls[$Vers[$i]];
-        }else{
-            $VersToText = $VersToText.' '.$Configs[0][($i-1)].' '.$Dts[$Vers[$i]].'= '.$Vls[$Vers[$i]];
+    foreach ($Vers as $key => $ver) {
+        if ($key === 0) {
+            $VersToText .= $Dts[$ver] . '= ' . $Vls[$ver];
+        } else {
+            $VersToText .= ' ' . $Configs[0][$key - 1] . ' ' . $Dts[$ver] . '= ' . $Vls[$ver];
         }
     }
-    $query = 'SELECT * FROM '.$Infs[0].' WHERE '.$VersToText;
+    $query = 'SELECT * FROM ' . $Infs[0] . ' WHERE ' . $VersToText;
     $res = $GLOBALS['conn']->query($query);
     $rows = mysqli_num_rows($res);
 
-    if($rows > 0){
+    if ($rows > 0) {
         mensage($Configs['ERROR'][0]);
-    }else if($rows == 0){
-        if(count($Up) > 0){
-            upFileinPage($Up);
-        }else{
-            $DtsToText = '';
-            for ($i=0; $i<count($Dts); $i++){
-                if($i==0){
-                    $DtsToText = $DtsToText.$Dts[$i];
-                }else{
-                    $DtsToText = $DtsToText.', '.$Dts[$i];
-                }
+    } else if ($rows == 0) {
+        if (count($Up) > 0) {
+            $NewLinkFiles = upFileinPage($Up, $Configs['FilesPast']);
+            $Vls[count($Vls) - 1] = $NewLinkFiles;
+        }
+        $DtsToText = implode(', ', $Dts);
+        $VlsToText = '';
+        foreach ($Vls as $key => $vl) {
+            if ($key === (count($Vls) - 1)) {
+                $VlsToText .= implode(', ', $vl);
+            } else {
+                $VlsToText .= $vl;
             }
-            $VlsToText = '';
-            for ($i=0; $i<count($Vls); $i++){
-                if($i==0){
-                    $VlsToText = $VlsToText.$Vls[$i];
-                }else{
-                    $VlsToText = $VlsToText.', '.$Vls[$i];
-                }
+            if ($key !== (count($Vls) - 1)) {
+                $VlsToText .= ', ';
             }
+        }
 
-            $query = 'INSERT INTO '.$Infs[0].' ( '.$DtsToText.' ) VALUES ( '.$VlsToText.' )';
-            $res = $GLOBALS['conn']->query($query);
-            if($res){
-                return GetBd($Infs, $Dts, $Vls);
-            }else{
-                mensage($Configs['ERROR'][1]);
-            }
+        $query = 'INSERT INTO ' . $Infs[0] . ' ( ' . $DtsToText . ' ) VALUES ( ' . $VlsToText . ' )';
+        $res = $GLOBALS['conn']->query($query);
+        if ($res) {
+            return GetBd($Infs, $Dts, $Vls);
+        } else {
+            mensage($Configs['ERROR'][1]);
         }
     }
 }
 
+function GetBd($Infs, $Dts, $Vls) {
+    $query = 'SELECT ';
+
+    if (empty($Dts)) {
+        $query .= '* ';
+    } else {
+        $query .= implode(', ', $Dts) . ' ';
+    }
+
+    $query .= 'FROM ' . $Infs[0] . ' WHERE ';
+    
+    $conditions = array();
+    foreach ($Dts as $dt) {
+        $conditions[] = $dt . " = '" . $Vls[$dt] . "'";
+    }
+    
+    $query .= implode(' AND ', $conditions);
+    $res = $GLOBALS['conn']->query($query);
+    
+    if($res){
+        $result = array();
+
+        while ($row = mysqli_fetch_assoc($res)) {
+            $result[] = $row;
+        }
+        
+        return $result;
+    }
+}
 function HeaderEcho($Title, $assets, $itemPlus) {
 		$res = '
 			<!DOCTYPE html>
